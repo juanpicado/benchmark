876281e498aaf890b6e752088ce9ded4da5c70d9
Juan Picado <juanpicado19@gmail.com>
2022-12-11 12:24:47 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const repo = 'benchmark'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 const nextConfig = {
   reactStrictMode: true,
