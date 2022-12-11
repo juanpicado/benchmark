af4347ce6f59fa4d88701a91cbbfa4498d214d13
Juan Picado <juanpicado19@gmail.com>
2022-12-11 12:23:14 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'benchmark'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig
